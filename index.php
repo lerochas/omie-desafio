fa448781692c69a71311f5f9dde759c4385ac25a
c1324535 LeandroRocha <c1324535@interno.bb.com.br>
2023-07-04 00:12:06 -0300

    wip

--- index.php
+++ index.php
@@ -10,3 +10,8 @@ use \App\Session\User as SessionUser;
 include SessionUser::isLogged() ?
     __DIR__ . '/includes/index.php' :
     __DIR__ . '/includes/login.php';
+
+//PARA VISUALIZAR A PÁGINA SEM PASSAR PELA TELA DE LOGIN,
+//DESCOMENTE AS LINHAS 16, 17 E COMENTE AS LINHAS 10, 11 E 12 
+// header('location: includes/index.php');
+// exit;
